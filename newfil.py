ea950160b7ab117b6d78679a5452e6a5d2650bac
artsiom31 <pervomdom@gmail.com>
2022-02-18 00:12:18 +0300

    aa

--- newfil.py
+++ newfil.py
@@ -6,9 +6,11 @@ pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((600, 400))
+
 rect(screen, (135, 206, 235), (0, 0, 600, 200))
 rect(screen, ("green"), (0, 200, 600, 200))
 
+# sky
 A = [(310, 40), (330, 40), (350, 40), (370, 40), (330, 25), (350, 25)]
 for x, y in A:
     circle(screen, ("white"), (x, y), 25)
@@ -16,10 +18,15 @@ for x, y in A:
 
 line(screen, ("black"), (420, 250), (420, 170), 15)
 
+#tree
 B = [(420, 125), (400, 140), (440, 140), (420, 155), (400, 170), (440, 170)]
 for x, y in B:
     circle(screen, (5, 102, 8), (x, y), 25)
     circle(screen, ("black"), (x, y), 25, 1)
+
+#sun
+circle(screen, ("yellow"), (500, 40), 30)
+
 
 
 
